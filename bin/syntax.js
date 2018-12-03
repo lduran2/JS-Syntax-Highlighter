c2149c90d322a9038b9e73047665a3fbe014ca9c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2018-12-02 23:24:15 -0500

    keyword highlighting

--- bin/syntax.js
+++ bin/syntax.js
@@ -1,4 +1,47 @@
+'use strict';
+
 (() => {
+	let javascript = {
+		"delimiters": [
+			{
+				"class": "block comment",
+				"begin": "/*",
+				"end": "*/"
+			},
+			{
+				"class": "end of line comment",
+				"begin": "//",
+				"end": ""
+			},
+			{
+				"class": "string-double",
+				"begin": "\"",
+				"end": "\""
+			},
+			{
+				"class": "string-single",
+				"begin": "'",
+				"end": "'"
+			},
+			{
+				"class": "template string",
+				"begin": "`",
+				"end": "`"
+			}
+		],
+		"literals": [
+			{
+				"class": "keyword",
+				"pattern": "\\b(break|case|catch|class|const|continue|debugger|default|delete|do|else|export|extends|finally|for|function|if|import|in|instanceof|new|return|super|switch|this|throw|try|typeof|var|void|while|with|yield|enum|implements|interface|let|package|private|protected|public|static|await|abstract|boolean|byte|char|double|final|float|goto|int|long|native|short|synchronized|throws|transient|volatile|null|true|false)\\b"
+			},
+			{
+				"class": "integer",
+				"pattern": "\\b\\d[0-9a-zA-Z]*"
+			},
+		]
+	};
+	let javascript_keywords = new RegExp(javascript.literals[0].pattern);
+
 	document.addEventListener('DOMContentLoaded', (e) => { 
 		Array.from(document.getElementsByClassName('program-listing'))
 			.forEach((el, k, arr) => {
@@ -18,22 +61,32 @@
 				const row = document.createElement('div');
 				row.className = 'row';
 				const new_line = (line || '\r\n');
+				let indent = 0;
 				new_line.split('')
-					.every(
-						(c, k, arr) => {
-							if (c === '\t') {
-								const tab = document.createElement('span');
-								tab.className = 'tab';
-								tab.appendChild(document.createTextNode('\t'));
-								row.appendChild(tab);
-								return true;
-							}
-							row.appendChild(document.createTextNode(new_line.substring(k)));
-							return false;
+					.every((c, k, arr) => {
+						if (c === '\t') {
+							const tab = document.createElement('span');
+							tab.className = 'tab';
+							tab.appendChild(document.createTextNode('\t'));
+							row.appendChild(tab);
+							return true;
 						}
-					)
+						indent = k;
+						return false;
+					});
+				new_line.substring(indent).split(javascript_keywords)
+					.forEach((s, k, arr) => {
+						if (javascript_keywords.test(s)) {
+							const keyword = document.createElement('span');
+							keyword.className = 'keyword';
+							keyword.appendChild(document.createTextNode(s));
+							row.appendChild(keyword);
+						}
+						else {
+							row.appendChild(document.createTextNode(s));
+						}
+					});
 				listing.appendChild(row);
 			});
-		console.log(listing.innerHTML);
 	}
 })();
